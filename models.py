878551e385198524ad80c374fd6d571a7a1b2a64
Lucas Vido <vidov8@gmail.com>
2016-10-14 14:17:52 -0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=128, default='')
+
+
+class Fluxograma(models.Model):
+    titulo = models.CharField(max_length=128, default='')
+    tarefas = models.ManyToManyField('Tarefa', through='TarefaFluxograma')
+
+
+class TarefaFluxograma(models.Model):
+    tarefa = models.ForeignKey(TemplateTarefa)
+    fluxograma = models.ForeignKey(Fluxograma)
+    numero = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ('numero',)
+        unique_together = ('tarefa', 'fluxograma', 'numero')
